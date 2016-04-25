1f96d388b5a2c9a0c3677cd6156fb06fe6868bd8
nikhilcodenomad <nikhil@codenomad.net>
2016-04-25 19:42:22 +0530

    Revert "adding files"
    This reverts commit a036d359f8271e52b3572fca30612c5ed4dc8a5a.

--- application/views/footer/slider.php
+++
@@ -1,8 +0,0 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="<?php echo base_url('media'); ?>/js/jquery-1.10.2.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="<?php echo base_url('media'); ?>/bs3/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url('media'); ?>/js/smooth-sliding-menu.js"></script> 
-<script src="<?php echo base_url('media'); ?>/js/jquery-ui-1.10.4.custom.min.js"></script> 
-<script src="<?php echo base_url('media'); ?>/js/sliders.js" type="text/javascript"></script> 
-<script src="<?php echo base_url('media'); ?>/plugins/knob/jquery.knob.min.js"></script> 
--- application/views/leftSideBar/index.php
+++
@@ -1,9 +0,0 @@
-<?php if($this->session->user_role_id != 11){
-	
-	$this->load->view('leftSideBar/other_user');
-	//prx($left_menu_detail);
-}else{
-	$this->load->view('leftSideBar/super_admin');
-}
-
-?>
